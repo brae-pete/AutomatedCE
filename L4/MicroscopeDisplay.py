341bb516bfbe1a6b820b4e07ec923a0b1650069d
Brae-pete <bvp391@gmail.com>
2020-06-24 16:22:59 -0700

    Camera Testing

--- L4/MicroscopeDisplay.py
+++ L4/MicroscopeDisplay.py
@@ -1,10 +1,19 @@
 """
 Modules for displaying images from a microscope. Live or snapped.
 """
+import queue
+import threading
 from abc import ABC, abstractmethod
+from queue import Queue
+
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+from L3.SystemsBuilder import CESystem
+from skimage.exposure import adjust_gamma
+
 
 class DisplayAbstraction(ABC):
-
     """
     Each Display should have a few basic commands.
 
@@ -15,6 +24,12 @@ class DisplayAbstraction(ABC):
     Display should be able to get a livestream of images.
     """
 
+    def __init__(self, system: CESystem):
+        self.gain = 1.0
+        self.gamma = 1.0
+        self.lock = threading.Lock()
+        self.system = system
+
     @abstractmethod
     def show(self):
         """
@@ -22,28 +37,16 @@ class DisplayAbstraction(ABC):
         :return:
         """
 
-    @abstractmethod
-    def set_brightness(self, brightness_scalar:float):
+    def adjust_gamma(self, gamma: float, gain=1.0):
         """
-        Sets the brightness to the pixel values passed in.
-        :param brightness_scalar: value to scale brightness by. when > 0, increase brightness.
+        Adjusts brightness of an image using Gamma correction. G
+        :param brightness_scalar: must be a nonnegative number
         :return:
         """
-
-    @abstractmethod
-    def get_brightness(self):
-        """
-        Returns the current setting of the brightness scalar
-        :return: brightness_scalar value where >0 increases brightness
-        :rtype: float
-        """
-
-    @abstractmethod
-    def set_contrast(self):
-        """
-        Sets the contrast of the image
-        :return:
-        """
+        with self.lock:
+            self.gamma = gamma
+            self.gain = gain
+        return True
 
     @abstractmethod
     def single_image(self):
@@ -59,4 +62,60 @@ class DisplayAbstraction(ABC):
         Retrieves a live stream of images from the source and continuously updates them in the window.
         :return:
         """
+
+
+class MatplotlibDisplay(DisplayAbstraction):
+
+    def __init__(self, system: CESystem):
+        super().__init__(system)
+        self.fig = None
+        self.img_ax = None
+        self.im = None
+        self.queue = Queue(maxsize=1)
+        self.system.camera.add_callback(self._update)
+        self._ani_func = None
+
+    def show(self):
+        self.fig = fig = plt.figure()
+        gs = fig.add_gridspec(10, 1)
+        self.img_ax = fig.add_subplot(gs[0:9])
+        self.img_ax.set_title("Camera View")
+        plt.show()
+        self.single_image()
+
+    def single_image(self):
+        img = self.system.camera.snap()
+        img = self._pre_plot_image(img)
+        with self.lock:
+            self.im = self.img_ax.imshow(img)
+
+    def _pre_plot_image(self, image):
+        image = adjust_gamma(image, gamma=self.gamma, gain=self.gain)
+        return image
+
+    def live_image(self):
+        self.system.camera.continuous_snap()
+        if self._ani_func is None:
+            self._ani_func = animation.FuncAnimation(self.fig, self._update, interval =100, blit=True)
+
+    def stop_live_image(self):
+        self.system.camera.stop()
+        self._ani_func = None
+
+    def _update(self, img):
+        try:
+            self.queue.put_nowait(img)
+        except queue.Full:
+            return
+
+    def _animate(self, *args):
+        try:
+            img = self.queue.get_nowait()
+            img = self._pre_plot_image(img)
+            self.im.set_array(img)
+            return self.im,
+
+        except queue.Empty:
+            return self.im,
+
 
